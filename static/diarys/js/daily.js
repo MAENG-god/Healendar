0d666b5de058893f8602b495eafd17b4f255094a
Kim mingi <kmk2737@snu.ac.kr>
2022-06-24 23:52:15 +0900

    edit_routine

--- static/diarys/js/daily.js
+++ static/diarys/js/daily.js
@@ -12,9 +12,9 @@ function add_routine(){
     var exercise = document.createElement("div");
     exercise.className = "routine_name";
     exercise.innerHTML = `
-    <span>Name:<input class="input_name" name="Name" type="text"></span>
-    <span> Sets:<input class="input_sets" name="sets" type="number" min="1"></span>
-    <input class="input_go" type="submit" value="GO!">
+    <span style="display:flex; margin: 0 0 0 15%;">Name:<input class="input_name" name="Name" type="text"></span>
+    <span style="display:flex; margin: 0 0 0 15%;"> Sets:<input class="input_sets" name="sets" type="number" min="1"></span>
+    <input class="input_go large_go" type="submit" value="GO!">
     `;
     var parent = document.querySelector(".add_table");
     if(parent.style.display == "flex"){
@@ -31,3 +31,8 @@ go_home.addEventListener("click", function(){
     location.href = "/diarys/"
 })
 
+var edit_routine = document.querySelector('.edit_routine');
+edit_routine.addEventListener("click", function(){
+    var header = edit_routine.parentNode;
+    
+})
